6542081fce49e5ea7d2ca61ef64e5a48b9378cc7
Kannan Pathy <kannan.pathy@aspiresys.com>
2018-02-01 12:50:50 +0530

    Updated the server code

--- Angular/server/server.ts
+++ Angular/server/server.ts
@@ -30,12 +30,18 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Solr Setup
 const client = new solrnode({
-    host: '172.31.100.48',
+    host: '172.24.213.57',
     port: '8085',
     core: 'report',
     protocol: 'http'
 });
 
+
+// Hold latest data and numRows
+let numRowsLatest, numRowsPrev;
+let latestData = {};
+
+// Solr query to get all records
 const solrGetAllQuery = client.query().q('*:*').start(0).rows(1000);
 
 // API to get all the data - localhost:3000/records
@@ -45,14 +51,24 @@ app.get('/records', (req, res) => {
             if (err) {
                 console.log('Error in fetching data: ' + err.msg + ' Code: ' + err.code);
             } else {
-                res.json(result.response);
+                numRowsPrev = parseInt(result.response.numFound);
+                console.log('All Data Rows: ' + numRowsPrev);
+                // const newResponse = result.response.docs.map((item) => {
+                //     item.id = parseInt(item.id);
+                // });
+                res.json(result.response.numFound);
+                // res.json(newResponse);
             }
         });
     }
     // setInterval(solrData, 2000);
+    solrData();
 });
 
-const solrGetLatest10uery = client.query().q('modified_time:[2018-01-30T16:00:00Z TO *]').sort({'modified_time':'desc'}).start(0).rows(10);
+// const solrGetLatest10uery = client.query().q('modified_time:[2018-01-30T16:00:00Z TO *]').sort({'modified_time':'desc'}).start(0).rows(10);
+// const solrGetLatest10uery = client.query().q('*:*').sort({'modified_time':'desc'});
+const solrGetLatest10uery = client.query().q('*:*').sort({'id':'desc'}).rows(10);
+
 
 // API to get only latest top 10 data - localhost:3000/recordsTop10
 app.get('/recordsTop10', (req, res) => {
@@ -61,11 +77,24 @@ app.get('/recordsTop10', (req, res) => {
             if (err) {
                 console.log('Error in fetching data: ' + err.msg + ' Code: ' + err.code);
             } else {
-                res.json(result.response);
+                numRowsLatest = parseInt(result.response.numFound);
+                // numRowsLatest = 606;                
+                console.log('All Data Rows New: ' + numRowsLatest);
+                if(numRowsLatest === numRowsPrev) {
+                    latestData = result.response.docs.filter((item) => {
+                        item.id = parseInt(item.id);
+                        return item.id > numRowsPrev;
+                    });
+                    
+                    numRowsPrev = numRowsLatest;
+                    res.json(latestData);
+                }
+                // res.json(result.response);
             }
         });
     }
-    setInterval(solrData, 1000);
+    // setInterval(solrData, 3000);
+    solrData();
 });
 
 // Start the server
